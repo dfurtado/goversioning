5f072ba57f7845313578d2e1a16f5f633399c9f3
Daniel Furtado <jazzmachine77@gmail.com>
2022-03-25 10:36:21 +0100

    Version 2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dfurtado/goversioning
+module github.com/dfurtado/goversioning/v2
 
 go 1.17
--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package hello
 import "fmt"
 
 func SayHello(name string) {
-	fmt.Printf("Hello, %s. This is the version 1", name)
+	fmt.Printf("Hello, %s. This is the version 2", name)
 }
