6bdaae92068fbab76e049d4290df128a43dd5644
Daniel Furtado <jazzmachine77@gmail.com>
2022-03-25 16:59:03 +0100

    new version

--- hello.go
+++ hello.go
@@ -3,5 +3,5 @@ package hello
 import "fmt"
 
 func SayHello(name string) {
-	fmt.Printf("Hello, %s. This is the version 1.0.1\n", name)
+	fmt.Printf("Hello, %s. This is the version 1.0.2\n", name)
 }
